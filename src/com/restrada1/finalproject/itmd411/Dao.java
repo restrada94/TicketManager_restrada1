07b30dad32f3b7cc3e6675511cb709f7acc210c5
restrada94 <35005338+restrada94@users.noreply.github.com>
2018-12-09 14:59:28 -0600

    Dao updates

---
+++ src/com/restrada1/finalproject/itmd411/Dao.java
@@ -0,0 +1,106 @@
+package com.restrada1.finalproject.itmd411;
+
+import java.sql.*;
+
+@SuppressWarnings("JpaQueryApiInspection")
+public class Dao {
+    /**
+     * Author: restrada1
+     * Class: Ticket.java
+     * Description:
+     * The TicketTemplate defines the high level behavior by defining abstract method signatures.
+     *
+     * To statically store Ticket objects, a static hashMap, TicketMap, will contain both the key and the value <String, TicketTemplate>.
+     */
+
+    static Connection connect = null;
+
+    {
+    CsvReader csvReader = new CsvReader();
+    }
+
+    public static Connection getConnection() {
+        // Setup the connection with the DB
+        try {
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://www.papademas.net:3307/tickets?autoReconnect=true&useSSL=false"
+                            + "&user=fp411&password=411");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connect;
+    }
+
+    ResultSet retrieveTicket(TicketTemplate ticket){
+
+        ResultSet rs = null;
+        PreparedStatement statement = null;
+
+        try{
+            String sql = "SELECT * FROM RE_TICKET_TABLE WHERE TICKET_ID = ?";
+            statement = getConnection().prepareStatement(sql);
+            statement.setString(1, ticket.getTicketID());
+            System.out.println("Executing SQL script...");
+            rs = statement.executeQuery();
+            // end create table
+            // close connection/statement object
+            statement.close();
+            connect.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    void createTicket(TicketTemplate ticket){
+        PreparedStatement statement = null;
+        try{
+            String sql = "SELECT * FROM RE_TICKET_TABLE WHERE TICKET_ID = ?";
+            statement = getConnection().prepareStatement(sql);
+            statement.setString(1, ticket.getTicketID());
+            System.out.println("Executing SQL script...");
+            statement.executeQuery();
+            // end create table
+            // close connection/statement object
+            statement.close();
+            connect.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    void updateTicket(TicketTemplate ticket){
+        PreparedStatement statement = null;
+        try{
+            String sql = "SELECT * FROM RE_TICKET_TABLE WHERE TICKET_ID = ?";
+            statement = getConnection().prepareStatement(sql);
+            statement.setString(1, ticket.getTicketID());
+            System.out.println("Executing SQL script...");
+            statement.executeQuery();
+            // end create table
+            // close connection/statement object
+            statement.close();
+            connect.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    void deleteTicket(TicketTemplate ticket){
+        PreparedStatement statement = null;
+        try{
+            String sql = "SELECT * FROM RE_TICKET_TABLE WHERE TICKET_ID = ?";
+            statement = getConnection().prepareStatement(sql);
+            statement.setString(1, ticket.getTicketID());
+            System.out.println("Executing SQL script...");
+            statement.executeQuery();
+            // end create table
+            // close connection/statement object
+            statement.close();
+            connect.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+}
