3e797e800b7ad75fca388e2148009019c442dbf3
restrada94 <restradadbr@gmail.com>
2018-12-10 22:43:00 -0600

    Login interface almost complete

--- src/com/restrada1/finalproject/itmd411/TicketManagerGUI.java
+++ src/com/restrada1/finalproject/itmd411/TicketManagerGUI.java
@@ -1,30 +1,98 @@
 package com.restrada1.finalproject.itmd411;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.SepiaTone;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class TicketManagerGUI extends Application {
 
+    Dao dao = new Dao();
+
     Stage primaryWindow;
-    Scene loginScreen, mainMenu, createTicketMenu, retrieveTicketMenu, updateTicketMenu, deleteTicketMenu, retrieveAllTicketsMenu;
+    Scene mainMenu, createTicketMenu, retrieveTicketMenu, updateTicketMenu, deleteTicketMenu, retrieveAllTicketsMenu;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryWindow = primaryStage;
+        try {
+            primaryWindow = primaryStage;
+            primaryWindow.setScene(loginScreenUI());
+            primaryWindow.setResizable(false);
+            primaryWindow.setTitle("ITMD 411 - Restrada1 TicketManager Final Project");
+            primaryWindow.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
-    void mainMenuUI(){}
+    Scene loginScreenUI(){
+        VBox vBox = new VBox(20);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setPadding(new Insets(25,50,20,50));
+        Scene scene = new Scene(vBox, 850, 600);
 
-    void createTicketMenuUI(){}
+        //Title fields
+        Text text = new Text("TicketManager Login");
+        text.setFont(Font.font("Courier New", FontWeight.BOLD, 24));
 
-    void retrieveTicketMenuUI(){}
+        //Login Fields
+        Label usernameLabel = new Label("Username:");
+        TextField usernameField = new TextField();
+        Label passwordLabel = new Label("Password:");
+        PasswordField passwordField = new PasswordField();
+        Label statusLabel = new Label("");
+        //Login Button
+        Button loginButton = new Button("Log In");
 
-    void updateTicketMenuUI(){}
+        //login Validation
+        if(loginValidator(usernameField, passwordField)) {
+            loginButton.setOnAction(e -> primaryWindow.setScene(mainMenuUI()));
+        } else {
+            loginButton.setOnAction(e -> statusLabel.setText("Invalid Credentials"));
+            statusLabel.setTextFill(Color.RED);
+            usernameField.setText("");
+            passwordField.setText("");
+        }
+        //Exit button
+        Button exitButton = new Button ("Exit");
+        exitButton.setOnAction(e -> Platform.exit());
 
-    void deleteTicketMenuUI(){}
+        //add components to the layout
+        vBox.getChildren().addAll(text, usernameLabel, usernameField, passwordLabel, passwordField, loginButton, exitButton, statusLabel);
 
-    void retrieveAllTicketsMenuUI(){}
+        return scene;
+    }
+
+    boolean loginValidator(TextField textField, PasswordField passwordField){
+        return textField.getText().equals("admin") && passwordField.getText().equals("admin");
+    }
+
+    Scene mainMenuUI(){ return null;}
+
+    Scene createTicketMenuUI(){return null;}
+
+    Scene retrieveTicketMenuUI(){return null;}
+
+    Scene updateTicketMenuUI(){return null;}
+
+    Scene deleteTicketMenuUI(){return  null;}
+
+    Scene retrieveAllTicketsMenuUI(){return null;}
 
     public static void main(String[] args){
         launch(args);
