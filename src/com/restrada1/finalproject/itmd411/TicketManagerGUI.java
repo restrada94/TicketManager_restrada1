24be65efe242d1c362ee71440f7e296a84b4f035
restrada94 <35005338+restrada94@users.noreply.github.com>
2018-12-11 22:31:45 -0600

    added methods

--- src/com/restrada1/finalproject/itmd411/TicketManagerGUI.java
+++ src/com/restrada1/finalproject/itmd411/TicketManagerGUI.java
@@ -2,13 +2,12 @@ package com.restrada1.finalproject.itmd411;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -17,11 +16,19 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.*;
 
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 public class TicketManagerGUI extends Application {
 
     private Dao dao = new Dao();
 
     private Stage primaryWindow;
+    private Stage popupStage;
+    private Stage miniPopupStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -145,11 +152,87 @@ public class TicketManagerGUI extends Application {
         vBox.setAlignment(Pos.CENTER);
         vBox.setPadding(new Insets(25,50,20,50));
 
+         //ID + "function" button Hbox pane + "Clear" button
+        Label idLabel = new Label("ID:");
 
+        TextField idField = new TextField();
+
+        Button functionButton = new Button("SUBSTITUTE HERE"); //TODO Replace Action, Program in functionality
+        functionButton.setOnAction(e -> {});
+        functionButton.setPrefSize(100, 20);
+
+        Button clearResults = new Button("Clear"); //TODO add functionality
+        clearResults.setOnAction(e -> {});
+        clearResults.setPrefSize(100, 20);
+
+        HBox hBox1 = new HBox(5);
+        hBox1.setAlignment(Pos.CENTER);
+        hBox1.getChildren().addAll(idField, functionButton, clearResults);
+
+        //Ticket object parameter descriptions (HBox)
+        Label id = new Label("ID");
+        id.setPrefSize(100,20);
+        id.setAlignment(Pos.CENTER);
+
+        Label customerName = new Label("Customer Name");
+        customerName.setPrefSize(100, 20);
+        customerName.setAlignment(Pos.CENTER);
+
+        Label dateTime = new Label("Date/Time");
+        dateTime.setPrefSize(100, 20);
+        dateTime.setAlignment(Pos.CENTER);
+
+        Label isResolved = new Label("Resolved?");
+        isResolved.setPrefSize(100, 20);
+        isResolved.setAlignment(Pos.CENTER);
+
+        Label priority = new Label("Priority");
+        priority.setPrefSize(100, 20);
+        priority.setAlignment(Pos.CENTER);
+
+        HBox hBox2 = new HBox(5);
+        hBox2.setPadding(new Insets(0,20,-8,20));
+        hBox2.setAlignment(Pos.CENTER);
+        hBox2.getChildren().setAll(id, customerName, dateTime, isResolved, priority);
+
+        //Ticket object parameter textboxes (HBox)
+        TextField idTextField = new TextField();
+        idTextField.setPrefSize(100,20);
+
+        TextField customerNameField = new TextField();
+        customerNameField.setPrefSize(100, 20);
+
+        TextField dateTimeField = new TextField();
+        dateTimeField.setPrefSize(100, 20);
+
+        TextField isResolvedField = new TextField();
+        isResolvedField.setPrefSize(100, 20);
+
+        TextField priorityField = new TextField();
+        priorityField.setPrefSize(100, 20);
+
+        HBox hBox3 = new HBox(5);
+        hBox3.setAlignment(Pos.CENTER);
+        hBox3.setPadding(new Insets(-8,20,0,20));
+        hBox3.getChildren().setAll(idTextField, customerNameField, dateTimeField, isResolvedField, priorityField);
+
+        //Description in VBox
+        Label descriptionLabel = new Label("Ticket Description");
+        descriptionLabel.setPadding(new Insets(-8, 20, -16, 20));
+        //Description Text box
+        TextArea descriptionField = new TextArea();
+        descriptionField.setPrefSize(400, 120);
+        descriptionField.setWrapText(true);
+        addJTextAreaLimiter(descriptionField, 250);
+        //"Exit" Button
+
+        Button exitButton = new Button("Exit");
+        exitButton.setPrefSize(100, 20);
+        exitButton.setOnAction(e -> popupStage.close());
 
         //creating a new scene.
         Scene scene = new Scene(vBox, 640, 480);
-        vBox.getChildren().addAll();
+        vBox.getChildren().addAll(hBox1, hBox2, hBox3, descriptionLabel, descriptionField, exitButton);
 
         return scene;
     }
@@ -216,13 +299,39 @@ public class TicketManagerGUI extends Application {
     }
     //adapter code for eliminating boilerplate and for creating a scene on a new popup window
     void createPopupWindow(Scene scene, String title){
-        Stage stage = new Stage();
+        popupStage = new Stage();
 
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setTitle(title);
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.setTitle(title);
 
-        stage.setScene(scene);
-        stage.showAndWait();
+        popupStage.setScene(scene);
+        popupStage.showAndWait();
+    }
+
+    //Method for defining a max setting for the number of allowed characters on a TextArea.
+    public void addTextAreaLimiter(final TextArea ta, final int maxLength) {
+        ta.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+                if (ta.getText().length() > maxLength) {
+                    String s = ta.getText().substring(0, maxLength);
+                    ta.setText(s);
+                }
+            }
+        });
+    }
+
+    //Method for defining a max setting for the number of allowed characters on a TextField.
+    public void addTextLimiter(final TextField tf, final int maxLength) {
+        tf.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+                if (tf.getText().length() > maxLength) {
+                    String s = tf.getText().substring(0, maxLength);
+                    tf.setText(s);
+                }
+            }
+        });
     }
 
     public static void main(String[] args){
